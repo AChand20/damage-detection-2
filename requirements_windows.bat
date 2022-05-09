7c04854e5d4566fd59bc9def604c4b372c284878
Abhishek Chand <abhishekchand62@gmail.com>
2022-05-09 20:34:49 +0530

    added requirement files

--- requirements_windows.bat
+++ requirements_windows.bat
@@ -1,5 +1,3 @@
 @echo off    
 pip install torch==1.10.0+cpu torchvision==0.11.0+cpu torchaudio==0.10.0 -f https://download.pytorch.org/whl/torch_stable.html
-git clone https://github.com/facebookresearch/detectron2.git
-python -m pip install -e detectron2
 exit /b
